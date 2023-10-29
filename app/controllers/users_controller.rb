f04774de1a6c9517fd83d0ed468306d1740bdfe3
Noriko <116807250+noriko-tahara@users.noreply.github.com>
2023-10-29 22:01:02 +0900

    ログイン,ログアウト機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-
+  skip_before_action :require_login, only: %i[new create]
+  
   def new
     @user = User.new
   end
@@ -8,7 +9,7 @@ class UsersController < ApplicationController
     @user =User.new(user_params)
 
     if @user.save
-      redirect_to  root_path
+      redirect_to  login_path
     else
       render :new
     end
