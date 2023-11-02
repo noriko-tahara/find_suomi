1521b6b00f6d1236d8656c2272a56784827d9ba5
Noriko <116807250+noriko-tahara@users.noreply.github.com>
2023-11-02 16:04:15 +0900

    エラーメッセージのカスタマイズ Userモデルのバリデーションに対するエラーメッセージを設定

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,7 @@ module FindSuomi
     config.i18n.available_locales = %i[ja en]
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
+    config.active_model.i18n_customize_full_message = true
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
