684d259dace34042fd9ff85c6052b4870b41fb55
Noriko <116807250+noriko-tahara@users.noreply.github.com>
2023-11-03 19:11:00 +0900

    ・gem webdriversによるエラーを解消するため削除 ・capybaraの設定

---
+++ spec/support/capybara.rb
@@ -0,0 +1,17 @@
+Capybara.register_driver :local_selenium_headless do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument("--headless")
+  options.add_argument("--window-size=1400,1400")
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+
+  config.before(:each, type: :system, js: true) do
+    driven_by :selenium_chrome_headless
+  end
+end
