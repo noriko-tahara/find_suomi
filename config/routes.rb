5ac74d5c2f19675ef8f05a5cccdb6dfae4b3b9d7
Noriko <116807250+noriko-tahara@users.noreply.github.com>
2023-11-03 12:55:42 +0900

    ユーザー登録ページへのURLを変更

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
+  get 'sign_up', to: 'users#new'
 
   resources :users, only: %i[new create]
 
