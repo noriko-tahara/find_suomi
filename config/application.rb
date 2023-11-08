eaa0d0d6f7f803f5b1c271366515cc53fa7f1c5a
Noriko <116807250+noriko-tahara@users.noreply.github.com>
2023-11-08 13:33:51 +0900

    Fix: 不要なテストファイルが作成されないようコードを修正

--- config/application.rb
+++ config/application.rb
@@ -14,11 +14,7 @@ module FindSuomi
       g.assets false
       g.helper false
       g.slip_routes true
-      g.test_framework :rspec,
-        fixtures: false,
-        view_specs: false,
-        helper_specs: false,
-        routing_specs: false
+      g.test_framework false
     end
 
     # Configuration for the application, engines, and railties goes here.
