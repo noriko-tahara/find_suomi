a7d927bca5a5cbed1d7cdecfba4d7ee1c37cbd01
Noriko <116807250+noriko-tahara@users.noreply.github.com>
2023-11-21 13:43:52 +0900

    Fix: rubocopの指摘事項を修正

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -12,10 +12,7 @@ class Facility < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
 
-  scope :by_genre, ->(genre_id) { joins(:facility_genres).where(facility_genres: { genre_id: genre_id }) }
-  scope :by_prefecture, ->(prefecture_id) { where(prefecture_id: prefecture_id) }
-  scope :keywords_contain, ->(word) {
-    where('name LIKE ?', "%#{word}%").or(
-    where('description LIKE ?', "%#{word}%")).or(
-    where('address LIKE ?', "%#{word}%")) }
+  scope :by_genre, ->(genre_id) { joins(:facility_genres).where(facility_genres: { genre_id: }) }
+  scope :by_prefecture, ->(prefecture_id) { where(prefecture_id:) }
+  scope :keywords_contain, ->(word) { where('name LIKE ? OR description LIKE ? OR address LIKE ?', "%#{word}%", "%#{word}%", "%#{word}%") }
 end
