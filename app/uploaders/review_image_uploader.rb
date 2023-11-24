019658c5b9c69d020506ece4f5ed223d31b16414
Noriko <116807250+noriko-tahara@users.noreply.github.com>
2023-11-24 18:18:36 +0900

    Add: 画像サイズ調整についての記述を追加

--- app/uploaders/review_image_uploader.rb
+++ app/uploaders/review_image_uploader.rb
@@ -1,7 +1,7 @@
 class ReviewImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -45,4 +45,8 @@ class ReviewImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
+
+  version :thumb do
+    process resize_to_fit: [150, 150]
+  end
 end
