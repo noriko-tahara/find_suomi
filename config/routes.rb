47799ec38e82fe13ff252191ea6cc758af967a58
Noriko <116807250+noriko-tahara@users.noreply.github.com>
2023-11-26 18:08:18 +0900

    Add: メール送信についての設定を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  mount LetterOpenerWeb::Engine, at: "letter_opener" if Rails.env.development?
   # Defines the root path route ("/")
   root "static_pages#top"
 
