f2ef72562252284f27105f4ce58775e8e40b061f
Noriko <116807250+noriko-tahara@users.noreply.github.com>
2023-11-28 10:22:48 +0900

    Add: 本番環境でのメール送信設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,9 +64,27 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = Settings.default_url_options.to_h
+
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  host = 'find-suomi.fly.dev'
+  config.action_mailer.default_url_options = { protocol: 'https', host: host}
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'smtp.gmail.com',
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_APPKEY'],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
