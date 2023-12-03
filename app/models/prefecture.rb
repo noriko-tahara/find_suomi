c3b16fe9b8fa7aedd6fc923697c467fb03fcff00
Noriko <116807250+noriko-tahara@users.noreply.github.com>
2023-12-03 14:08:56 +0900

    Fix: RuboCopの指摘事項を修正

--- app/models/prefecture.rb
+++ app/models/prefecture.rb
@@ -2,5 +2,5 @@ class Prefecture < ApplicationRecord
   has_many :facilities
   validates :name, presence: true
 
-  scope :selected_prefectures, -> { where(name: ['東京都', '神奈川県', '埼玉県', '千葉県']) }
+  scope :selected_prefectures, -> { where(name: %w[東京都 神奈川県 埼玉県 千葉県]) }
 end
