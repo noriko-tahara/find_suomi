e312b099000dd26c4a710b7c82d04cf91b9d3387
Noriko <116807250+noriko-tahara@users.noreply.github.com>
2023-12-09 16:17:12 +0900

    Add: likesコントローラーを作成

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,11 @@
+class LikesController < ApplicationController
+  def create
+    @review = Review.find(params[:review_id])
+    current_user.like(@review)
+  end
+
+  def destroy
+    @review = current_user.likes.find(params[:id]).review
+    current_user.unlike(@review)
+  end
+end
